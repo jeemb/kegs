785a7aa67e08ac58a401daf2ca4c37e40dbb7fc4
Clayton Collins and Jennifer Beem <jennifer@>
2017-03-28 10:43:12 -0700

    remove funcitons to create a blank template

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,23 +4,15 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template:`
   <div class='container'>
-    <h1>Recipies</h1>
-    <h3> here are your stupid recipies </h3>
-    <ul>
-      <li *ngFor="let recipie of recipies">{{recipie.description}} needs {{recipie.ingredients}}</li>
-    </ul>
+    <h1>Hello</h1>
   </div>
   `
 })
 
 export class AppComponent {
-  recipies: Recipie[] = [
-    new Recipie("pasta", ['noods','sauce','italyf']),
-    new Recipie("shrimp", ['water','fish','small eyes'])
-  ];
+
 }
 
 
 export class Recipie {
-  constructor(public description: string, public ingredients: array) { }
 }
